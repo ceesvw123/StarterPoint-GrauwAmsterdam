7bfe3908dcdcffb02798383442d4781508ae4041
iceclaser <mikeweer@hotmail.com>
2016-11-10 10:06:46 +0100

    fixed shit

--- view/contact.php
+++ view/contact.php
@@ -27,3 +27,10 @@
 
 
 </div>
+
+
+</div>
+
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="js/scroll.js"></script>
+<script src="js/script.js"></script>
