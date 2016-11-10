b197a684a0b421044f0d0ced8b178686b28fe404
iceclaser <mikeweer@hotmail.com>
2016-11-10 16:24:32 +0100

    visie vouw compleet

--- view/home.php
+++ view/home.php
@@ -1,14 +1,16 @@
     <div class="header" id="header">
       <div class="headerImg">
-        <div class="headerText">
-            <h1>Grauw Amsterdam</h1>
-        </div>
           <img src="img/menenwoman.png" class="headerPic"alt="man en vrouw" />
+      </div>
+      <div class="headerText">
+          <h1>Grauw Amsterdam</h1>
       </div>
         <p class="visie">Er is maar één schoonheid te vinden in de kleur zwart. GRAUW ziet de kracht in
 
 de glans van de zwarte pantervacht. GRAUW draait de hand niet om voor wat
 
 permanente inkt en gelooft in de duisterheid van Darth Vader.</p>
-   
+<a href="#kleding">
+  <button type="button" name="button" class="headerButton">kleding</button>
+</a>
     </div>
